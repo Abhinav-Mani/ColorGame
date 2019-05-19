d4e399490f2bb5601dfd7a339c831026131439ac
Abhinav-Mani <47060625+Abhinav-Mani@users.noreply.github.com>
2019-05-19 16:25:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
 var boxcount=6;
+var rc;
+boxsetup();
+easy.addEventListener("click",function(){
+    boxcount=3;
+    document.querySelector("#downbox").style.display="none";
+    boxsetup();
+});
+hard.addEventListener("click",function(){
+    boxcount=6;
+    document.querySelector("#downbox").style.display="block";
+    boxsetup();
+});
+function generaterandomcolor(num)
+{
+    var c=[];
+    for(var i=0;i< num;i++)
+    {
+        c[i]=getcol();
+    }
+    return c;
+}
+function getcol()
+{
+    return "rgb("+Math.floor(Math.random()*256)+", "+Math.floor(Math.random()*256)+", "+Math.floor(Math.random()*256)+")";
+}
+function boxsetup()
+{
 var hard=document.querySelector("#hard");
 var easy=document.querySelector("#easy");
 var colors=generaterandomcolor(boxcount);
 var boxes= document.querySelectorAll("#box div div");
 var header=document.querySelector("#top1");
 var play=document.querySelector("#play");
-for(var i=0;i< boxes.length;i++)
+for(var i=0;i< boxcount;i++)
 {
     boxes[i].style.backgroundColor=colors[i];
-}
-var rc=colors[Math.floor(Math.random()*boxcount)];
-document.querySelector("#top1").textContent=rc;
-for(var i=0;i< boxes.length;i++)
-{
-    boxes[i].addEventListener("click",clickhandle)
-}
-function clickhandle()
-{
-    if(this.style.backgroundColor == rc)
+    boxes[i].addEventListener("click",function(){
+        if(this.style.backgroundColor == rc)
     {
         header.style.background="linear-gradient("+rc+","+"#fff"+")";
         for(var i=0;i< boxes.length;i++)
@@ -33,18 +52,8 @@ function clickhandle()
         this.style.border="none";
         play.textContent="OPssie! TRy aGain!";
     }
-
+    });
 }
-function generaterandomcolor(num)
-{
-    var c=[];
-    for(var i=0;i< num;i++)
-    {
-        c[i]=getcol();
-    }
-    return c;
-}
-function getcol()
-{
-    return "rgb("+Math.floor(Math.random()*256)+", "+Math.floor(Math.random()*256)+", "+Math.floor(Math.random()*256)+")";
+rc=colors[Math.floor(Math.random()*boxcount)];
+document.querySelector("#top1").textContent=rc;
 }
